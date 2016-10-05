fe5bdd35eb0548cda1d1756eb6b65aff9cd4aa14
smwinzer <biol456-31@biolab1041.local>
2016-10-04 17:39:39 -0700

    Completion HW4
    Finished HW4. Python code works and output has been sent to HW output
    file. Not submitted on shared repo.

--- HW4/reformatReads.py
+++ HW4/reformatReads.py
@@ -2,4 +2,35 @@
 
 import sys
 
-#
+#Read from STDIN
+
+Headers = sys.stdin.readlines()
+
+#Split each line input into separate fields
+
+for nextHeader in Headers:
+	Fields = nextHeader.split()
+	Technology, sampleID, Primers, gene = Fields
+	
+	#Extract technology type
+	Technology = Technology.split(':')
+	Technology = Technology[0][1:]
+
+	#Extract forward and reverse primers
+	Primers = Primers.split('|')
+	fwdPrimer, revPrimer = Primers[0], Primers[2]
+
+	#Extract gene name
+	gene = gene.split('|')
+	gene = gene[0]
+	
+	#Extract country of origin
+	Origin = sampleID.split(':')
+	Origin = Origin[3][:2]
+
+	#Create sampleID
+	sampleID = sampleID.split(':')
+	sampleID = sampleID[3]+'_'+gene
+	
+	#print all fields separated by tabs
+	print("SampleID: %s\tTech: %s\tfwd: %s\trev: %s\tGene: %s\tOrigin: %s\n" % (sampleID,Technology,fwdPrimer,revPrimer,gene,Origin))
