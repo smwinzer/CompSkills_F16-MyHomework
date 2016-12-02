5aee1b00eab9056f480aea73c3045826339fe16d
smwinzer <biol456-31@biolab1041.local>
2016-12-02 02:31:12 -0800

    Finished HW9
    Not fully complete, but as complete as it’s going to get.

--- HW9/plots.py
+++ HW9/plots.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python
 
 #This is the code for HW9 which generates figures from the results of previous assignments.
 
@@ -11,34 +11,19 @@ from pylab import figure, axes, pie, title, show
 #Make horizontal stacked barcharts of each woman's microbiome using summaryByWoman.txt
 
 data = pd.read_csv('summaryByWoman.txt', sep = '\t', comment='#')
+data = data.sort(columns='patientID',ascending=False)
 data = data.drop(['Total','Shannon','Simpson'], axis=1)
 fig = data.plot(kind = 'barh',stacked=True)
+plt.legend(loc='center right',prop={'size':10})
+plt.xlim([0,2])
 fig.set_yticklabels(data[data.columns[0]])
-
+fig.set_xticks([0.1,0.3,0.5,0.7,0.9])
+fig.set_xlabel('Relative Abundance', fontsize = 'large')
+fig.set_ylabel('Patient Identifier', fontsize = 'large')
+fig.set_title('Microbiome composition by woman', fontsize = 'x-large')
 
 #Save figure as MicrobiomeByWoman.png
 
-fig.figure.savefig('test.png')
-
-#Something you were playing with that kinda failed
-
-fig, axes = plt.subplots(2,2)
-ax = axes[0,0]
-
-fig = deciles.plot( ax=ax, kind='bar',color='blue')
-ax = axes[0,1]
-fig = deciles.plot( ax=ax, kind='barh',color='yellow')
-
-fig.set_tight_layout(True)
-fig.savefig('test.png')
-
-fig.figure.savefig('test.png') # THIS WORKS!!!!
-
-summaryByDay = summaryByDay.drop(['ID','time','sampleID','Day','Date','Batch_ID'],axis=1)
+fig.figure.savefig('MicrobiomeByWoman.png')
 
 
-
-
-
-
-fig.show()
